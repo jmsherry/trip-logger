519b9b50d23783e73ba415865c97627ff82b3846
James Sherry <james.m.sherry@googlemail.com>
2022-08-25 03:32:17 +0100

    initial files

---
+++ server/server.js
@@ -0,0 +1,14 @@
+import express from 'express';
+import './database.js';
+import middlewareSetup from './middleware.js';
+import setupRoutes from './routes/index.js';
+
+const {} = process.env;
+
+const app = express();
+
+middlewareSetup(app);
+setupRoutes(app);
+
+
+export default app;
---
+++ snippets.md
@@ -0,0 +1,2 @@
+1. <https://github.com/jmsherry/c10-birds/blob/main/server/index.js>
+2. <https://github.com/jmsherry/c10-birds/blob/main/server/server.js>
