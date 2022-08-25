fe0f68ca187dd325d9a47526cc946425b1dcebdc
James Sherry <james.m.sherry@googlemail.com>
2022-08-25 16:55:35 +0100

    forgot new files

---
+++ client/src/pages/AddTrips.jsx
@@ -0,0 +1,9 @@
+import React, {useState, useEffect} from 'react'
+
+function Trips() {
+  return (
+    <div>Trips</div>
+  )
+}
+
+export default Trips
---
+++ client/src/pages/Profile.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import {Link} from 'react-router-dom'
+import { useAuth0 } from "@auth0/auth0-react";
+
+const Profile = () => {
+  const { user, isAuthenticated, isLoading } = useAuth0();
+
+  if (isLoading) {
+    return <div>Loading ...</div>;
+  }
+
+  return (
+    isAuthenticated && (
+      <div>
+        <img src={user.picture} alt={user.name} />
+        <h2>{user.name}</h2>
+        <p>{user.email}</p>
+        <pre><code>{JSON.stringify(user, null, 2)}</code></pre>
+        <section>
+          <h2>Trips</h2>
+          <Link to="/trips">See your trips</Link>
+        </section>
+      </div>
+    )
+  );
+};
+
+export default Profile;
