d4da4db7d6b3b1b2f8ecb551b3bff4377ad13f45
James Sherry <james.m.sherry@googlemail.com>
2022-08-25 17:02:53 +0100

    removed comment

--- client/src/components/ProtectedRoute.jsx
+++ client/src/components/ProtectedRoute.jsx
@@ -1,7 +1,3 @@
-// const ProtectedRoute = ({ component, ...args }) => (
-//   <Route component={withAuthenticationRequired(component)} {...args} />
-// );
-
 import React from "react";
 import { Navigate } from "react-router-dom";
 import { useAuth0 } from "@auth0/auth0-react";
